e9188e2baad4d1daaaec9dbed74b61bdaacd2453
Gerner Balázs <Gerner@DESKTOP-870G18F.local>
2018-03-08 19:37:04 +0100

    couchbase lite mock

---
+++ src/persistence/LibraryScannerUtilMock.java
@@ -0,0 +1,38 @@
+package persistence;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.io.FilenameUtils;
+
+import model.MusicFile;
+
+public class LibraryScannerUtilMock {
+
+  protected static final String LIBRARY_PATH = "C:\\Users\\Gerner\\Music";
+
+  protected List<String> supportedExtensions = Arrays.asList("mp3", "m4a", "flac");
+
+  protected List<MusicFile> model = new ArrayList<MusicFile>();
+
+  protected List<MusicFile> getModel() {
+    return model;
+  }
+
+  protected void scanLibrary(File library) throws IOException {
+    for (File file : library.listFiles()) {
+      if (file.isDirectory()) {
+        scanLibrary(file);
+      } else {
+        String ext = FilenameUtils.getExtension(file.getName());
+        if (supportedExtensions.contains(ext)) {
+          model.add(new MusicFile(file));
+        }
+      }
+    }
+  }
+
+}
--- src/persistence/mongo/MongoClientMain.java
+++ src/persistence/mongo/MongoClientMain.java
@@ -1,4 +1,4 @@
-package mongo;
+package persistence.mongo;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
@@ -6,25 +6,15 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-import org.apache.commons.io.FilenameUtils;
 import org.bson.codecs.configuration.CodecRegistries;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
 
 import model.MusicFile;
+import persistence.LibraryScannerUtilMock;
 
-public class MongoClientMain {
-
-  private static final String LIBRARY_PATH = "C:\\Users\\Gerner\\Music";
-
-  private List<String> supportedExtensions = Arrays.asList("mp3", "m4a", "flac");
-
-  private List<MusicFile> model = new ArrayList<MusicFile>();
+public class MongoClientMain extends LibraryScannerUtilMock {
 
   // mongodemo
   public static void main(String[] args) {
@@ -46,23 +36,6 @@ public class MongoClientMain {
       e.printStackTrace();
     }
 
-  }
-
-  private List<MusicFile> getModel() {
-    return model;
-  }
-
-  public void scanLibrary(File library) throws IOException {
-    for (File file : library.listFiles()) {
-      if (file.isDirectory()) {
-        scanLibrary(file);
-      } else {
-        String ext = FilenameUtils.getExtension(file.getName());
-        if (supportedExtensions.contains(ext)) {
-          model.add(new MusicFile(file));
-        }
-      }
-    }
   }
 
 }
